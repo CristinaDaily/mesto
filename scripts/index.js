2fea91b0ad8993b635cb6e89d098a7b6b9497369
Cristina <kristi.chichkanova@gmail.com>
2023-05-04 21:26:52 +0200

    fix:open/close popup functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,40 @@
-const editButton = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const popupCloseButton = document.querySelector(".popup__close");
-const formElement = document.querySelector(".popup__form");
-const nameInput = formElement.querySelector(".popup__item_el_name");
-const jobInput = formElement.querySelector(".popup__item_el_about");
-const profileName = document.querySelector(".profile__name");
-const profileOccupation = document.querySelector(".profile__occupation");
-const saveButton = document.querySelector(".popup__save-btn");
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close');
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__item_el_name');
+const jobInput = formElement.querySelector('.popup__item_el_about');
+const profileName = document.querySelector('.profile__name');
+const profileOccupation = document.querySelector('.profile__occupation');
 
-const togglePopupStatus = function (popupToToggle) {
-  popupToToggle.classList.toggle("popup_opened");
+const openPopup = function (popupToOpen) {
+  popupToOpen.classList.add('popup_opened');
+
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileOccupation.textContent;
+};
+
+const closePopup = function (popupToClose) {
+  popupToClose.classList.remove('popup_opened');
+
+  profileName.textContent = nameInput.value;
+  profileOccupation.textContent = jobInput.value;
 };
 
 //Откытие поп-ап через кнопку релдактирование
-editButton.addEventListener("click", () => {
-  togglePopupStatus(popup);
+editButton.addEventListener('click', () => {
+  openPopup(popup);
 });
 
 // Закрытие поп-ап
-popupCloseButton.addEventListener("click", () => {
-  togglePopupStatus(popup);
+popupCloseButton.addEventListener('click', () => {
+  closePopup(popup);
 });
 
 //Обработчик «отправки» формы
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
-  profileName.textContent = nameInput.value;
-  profileOccupation.textContent = jobInput.value;
-
-  togglePopupStatus(popup);
+  closePopup(popup);
 }
 
-formElement.addEventListener("submit", handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);
