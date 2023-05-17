ca14bb23c5bbc6d5f10f219b8ff99e0997144840
Cristina <kristi.chichkanova@gmail.com>
2023-05-17 21:05:25 +0200

    add initial cards js

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,44 @@ const nameInput = formElement.querySelector('.popup__item_el_name');
 const jobInput = formElement.querySelector('.popup__item_el_about');
 const profileName = document.querySelector('.profile__name');
 const profileOccupation = document.querySelector('.profile__occupation');
+const elements = document.querySelector('.elements');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+// Adding initial 6 cards
+initialCards.forEach((item) => {
+  const elementTemplate = document.querySelector('#element-template').content;
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__image').src = item.link;
+  element.querySelector('.element__place-name').textContent = item.name;
+
+  elements.append(element);
+});
 
 const openPopup = function (popupToOpen) {
   popupToOpen.classList.add('popup_opened');
